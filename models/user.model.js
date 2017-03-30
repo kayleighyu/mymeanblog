978575e46ee93f33d5c2957cdc0b581ca376e694
Kayleigh Yu <kayleighyu@gmail.com>
2017-03-29 20:21:36 -0400

    we implemented the setPassword, validPassword, and generateJwt functions on the user model. We added the node module jsonwebtoken to the dependencies for our application

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const signature = process.env.SIGNATURE || require('../secrets').SIGNATURE;
 
-var userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   email:{
     type: String,
     required: true,
@@ -18,9 +21,24 @@ var userSchema = mongoose.Schema({
   }
 });
 
-userSchema.methods.setPassword = function(password){}; //set salt and hash on user
-userSchema.methods.validPassword = function(password){};
-userSchema.methods.generateJwt = function(){};
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64);
+}; //set salt and hash on user
+userSchema.methods.validPassword = function(password){
+  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64);
+  return this.hash === hash; ///if they equal, ye shall pass, otherwise, get out
+};
+userSchema.methods.generateJwt = function(){
+  const expiration = new Date();
+  expiration.setDate(expiration.getDate() + 7); //create a date 7 days in the future
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiration.gettime() / 1000) //paseInt ensures no fraction
+  }, signature);
+};
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
